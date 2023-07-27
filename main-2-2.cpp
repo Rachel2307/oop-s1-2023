25ce3158017afbb985d1c99884a7cb43074c1125
RachelUniAde <hongocthanhuyen@gmail.com>
2023-07-27 20:02:26 +0930

    practical 2.2

---
+++ main-2-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int max_element(int array[], int n);
+int main() {
+    int array[5] = {1,2,-5,4,3};
+    cout<<"The array is: ";
+    for (int i=0;i<5;i++) {
+        cout<<array[i]<<" ";
+    }
+    cout<<"\nThe maximum number is "<< max_element(array,5)<<endl;
+    return 0;
+}
