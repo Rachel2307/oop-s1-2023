2a0bd7ca9731f7bf56bfde889f72ff02f735f044
RachelUniAde <hongocthanhuyen@gmail.com>
2023-07-28 00:45:41 +0930

    practical 2.4

---
+++ function-2-3.cpp
@@ -0,0 +1,20 @@
+ void two_five_nine(int array[], int n) {
+    int count2=0,count5=0,count9=0;
+    for (int i=0;i<n;i++) {
+        switch(array[i]) {
+            case 2:
+                count2++;
+                break;
+            case 5:
+                count5++;
+                break;
+            case 9:
+                count9++;
+                break;  
+        }
+    }
+    // display the counts
+    cout<<"2:"<<count2;
+    cout<<";5:"<<count5;
+    cout<<";9:"<<count9;
+ }
