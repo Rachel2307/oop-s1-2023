bc5f59b1cebedfd263cef27184c1d7648b79ae2c
RachelUniAde <hongocthanhuyen@gmail.com>
2023-07-27 19:57:41 +0930

    practical 2.1

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
-int num_count(int array[], int n, int number);
+int min_element(int array[], int n);
 int main() {
     int array[5] = {1,2,-5,4,3};
     cout<<"The array is: ";
     for (int i=0;i<5;i++) {
         cout<<array[i]<<" ";
     }
-    cout<<"\nThe count of is "<< num_count(array,5,3)<<endl;
+    cout<<"\nThe minimum number is "<< min_element(array,5)<<endl;
     return 0;
 }
