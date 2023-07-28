799bc5ea21cae8cfef9e18509514ab48f0602ae9
RachelUniAde <hongocthanhuyen@gmail.com>
2023-07-28 13:38:17 +0930

    practical 2.3

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -1,5 +1,5 @@
+ #include <iostream>
  void two_five_nine(int array[], int n) {
-    #include <iostream>
     int count2=0,count5=0,count9=0;
     for (int i=0;i<n;i++) {
         switch(array[i]) {
@@ -17,5 +17,5 @@
         }
     }
     // display the counts
-    cout<<"2:"<<count2<<";"<<"5:"<<count5<<";"<<"9:"<<count9<<";";
+    std::cout<<"2:"<<count2<<";"<<"5:"<<count5<<";"<<"9:"<<count9<<";"<< std::endl;
  }
