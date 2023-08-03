1e857f0ebee6483a6f74d5b9bb91491eb5bee85f
RachelUniAde <hongocthanhuyen@gmail.com>
2023-08-04 01:31:26 +0930

    1st commmit

---
+++ function-1-4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void print_scaled(int array[3][3],int scale) {
+    // multiply matrix by a specific scale
+    for (int i=0;i<3;i++) {
+        for (int j=0;j<3;j++) {
+            array[i][j]=array[i][j]*scale;
+        }
+    }
+    // display result
+    for (int i=0;i<3;i++) {
+        for (int j=0;j<3;j++) {
+            cout<<array[i][j]<<" ";
+        }
+        cout<<endl;
+    } 
+}
