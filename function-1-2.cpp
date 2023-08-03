3b8081da6a36f8bdb787dfd02cbf1d0503aa9766
RachelUniAde <hongocthanhuyen@gmail.com>
2023-08-04 01:04:54 +0930

    modify for loop of function

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,6 +1,6 @@
 int is_identity(int array[10][10]) {
-    for (int i=0;i<3;i++) {
-        for (int j=0;j<3;j++) {
+    for (int i=0;i<10;i++) {
+        for (int j=0;j<10;j++) {
             if ((i==j) && array[i][j]!=1) {
                 return 0;
             } else if ((i!=j) && array[i][j]!=0) {
