4ccf5eb030f00092239fa74d63e8e354850867e0
RachelUniAde <hongocthanhuyen@gmail.com>
2023-08-04 01:01:56 +0930

    modify code in function

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,17 +1,12 @@
 int is_identity(int array[10][10]) {
-    int determinant = 0;
     for (int i=0;i<3;i++) {
         for (int j=0;j<3;j++) {
             if ((i==j) && array[i][j]!=1) {
-                determinant=0;
-                break;
+                return 0;
             } else if ((i!=j) && array[i][j]!=0) {
-                determinant=0;
-                break;
-            } else {
-                determinant=1;
+                return 0;
             }
         }
     }
-    return determinant;
+    return 1;
 }
