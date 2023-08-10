142fcfb7d029954db57b89c3cea900d54e342a2f
RachelUniAde <hongocthanhuyen@gmail.com>
2023-08-10 15:59:27 +0930

    1st commit

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 void print_matrix(int array[10][10]);
 int main() {
-    int arrays[3][3] = {{1,2,3},{1,2,3},{1,2,3}};
-    cout<<print_matrix(array)<<endl;
+    int arrays[10][10] = {{1,2,3},{1,2,3},{1,2,3}};
+    print_matrix(arrays);
+    return 0;
 }
