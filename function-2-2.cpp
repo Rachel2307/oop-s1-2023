35a4fa757001407c10d9a490cd288faef333ce23
RachelUniAde <hongocthanhuyen@gmail.com>
2023-08-10 16:03:10 +0930

    2nd commit

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -8,21 +8,4 @@ int bin_to_int(int binary_digits[], int number_of_digits) {
         power = power + 1;
     }
     return integer;
-
-    //binaryNumber = "";
-    //while (decimal > 0) {
-      //  int remainder = decimal % 2;
-        //binaryNumber = std::to_string(remainder) + binaryNumber;
-        //decimal = decimal / 2;
-   // }
-   //return binaryNumber;
 }
-
-
-//int main() {
-    // Test decimalToBinary function
-    //int decimalNum = 75;
-    //std::string binaryResult = decimalToBinary(decimalNum);
-   //std::cout << "Decimal " << decimalNum << " in binary: " << binaryResult << std::endl;
-    //return 0;
-//}
--- main-2-3.cpp
+++ main-2-3.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 using namespace std;
-void print_matrix(int array[10][10]);
+int palindrome_sum(int integers[], int length);
+bool is_array_palindrome(int integers[], int length);
+int sum_integers(int integers[], int length);
 int main() {
-    int arrays[3][3] = {{1,2,3},{1,2,3},{1,2,3}};
-    cout<<print_matrix(array)<<endl;
+    int arrays[6] = {1,2,0,0,2,1};
+    int size = 6;
+    cout<<palindrome_sum(arrays,size)<<endl;
 }
