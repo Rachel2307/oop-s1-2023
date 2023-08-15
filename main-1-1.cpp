0db443f6dbc9149cfab642edca48f5a94e7319d1
RachelUniAde <hongocthanhuyen@gmail.com>
2023-08-15 13:32:55 +0930

    delete prac_exam from workshop 4

--- main-1-1.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-void print_matrix(int array[10][10]);
-int main() {
-    int arrays[10][10] = {{1,2,3},{1,2,3},{1,2,3}};
-    print_matrix(arrays);
-    return 0;
-}
