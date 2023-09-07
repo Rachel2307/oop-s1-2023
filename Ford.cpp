3787a34c7c9edeb8c9b4bef37f62c6f3b942853a
RachelUniAde <hongocthanhuyen@gmail.com>
2023-09-07 16:48:41 +0930

    change

--- Ford.cpp
+++ Ford.cpp
@@ -1,6 +1,6 @@
 #include "Ford.h"
 
-Ford::Ford() : Car(), badgeNumber(0), litresOfFuel(60.0) {}
+Ford::Ford() : badgeNumber(0), litresOfFuel(60.0) {}
 
 Ford::Ford(int badgeNumber, int price) : Car(price), badgeNumber(badgeNumber), litresOfFuel(60.0) {}
 
@@ -8,36 +8,40 @@ int Ford::get_badgeNumber() const {
     return badgeNumber;
 }
 
-void Ford::set_badgeNumber(int badgeNumber) {
-    this->badgeNumber = badgeNumber;
-}
-
 float Ford::get_litresOfFuel() const {
     return litresOfFuel;
 }
 
+void Ford::set_badgeNumber(int badgeNumber) {
+    this->badgeNumber = badgeNumber;
+}
+
 void Ford::set_litresOfFuel(float litresOfFuel) {
-    this->litresOfFuel = litresOfFuel;
+    if (litresOfFuel < 0.0)
+        this->litresOfFuel = 0.0;
+    else if (litresOfFuel > 60.0)
+        this->litresOfFuel = 60.0;
+    else
+        this->litresOfFuel = litresOfFuel;
 }
 
 void Ford::refuel(int litres) {
-    litresOfFuel += litres;
-    if (litresOfFuel > 60.0) {
+    if (litresOfFuel + litres > 60.0)
         litresOfFuel = 60.0;
-    }
+    else
+        litresOfFuel += litres;
 }
 
 void Ford::drive(int kms) {
     float co2Emissions = 234.0 * kms;
-    Car::drive(kms);
-    if (litresOfFuel >= 5.0) {
+    if (litresOfFuel >= 5.0 * static_cast<float>(kms) / 5.0) {
         litresOfFuel -= static_cast<float>(kms) / 5.0;
         if (litresOfFuel < 0.0)
             litresOfFuel = 0.0;
     }
     else {
-        litresOfFuel = 0.0;
         co2Emissions += 234.0 * litresOfFuel;
+        litresOfFuel = 0.0;
     }
     set_emissions(get_emissions() + static_cast<int>(co2Emissions));
 }
