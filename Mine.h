36dbee368a50dfc146c891b7dbcbca889eff0769
RachelUniAde <hongocthanhuyen@gmail.com>
2023-10-24 17:55:13 +1030

    11th

--- Mine.h
+++ Mine.h
@@ -8,8 +8,16 @@ public:
     Mine(int x, int y) : GameEntity(x, y, 'M') {}
 
     Explosion explode() {
+        // Set the mine's type to 'X' to mark it as exploded
+        setType('X');
+        // Create and return an Explosion object
+        //return Explosion(getPos());
         return Explosion(std::get<0>(position), std::get<1>(position));
     }
+    
+    /*Explosion explode() {
+        return Explosion(std::get<0>(position), std::get<1>(position));
+    }*/
 
     // Override the update function
     void update() override {}
