6c5257c5bf6524d2f699c4579cd181e427f8091e
RachelUniAde <hongocthanhuyen@gmail.com>
2023-10-26 15:59:42 +1030

    8th

--- GameSession.h
+++ GameSession.h
@@ -66,15 +66,4 @@ public:
     }
 };
 
-int main() {
-    GameSession game;
-    game.initGameSession(2, 3, 10, 5);
-    game.gameCycle(20, 2.0);
-    
-    // Clean up allocated memory
-    for (GridUnit* unit : game.getGrid()) {
-        delete unit;
-    }
 
-    return 0;
-}
--- main-3.cpp
+++ main-3.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-#include "Obstacle.h"
-#include "Avatar.h"
-
+#include "GameSession.h"
 int main() {
+    GameSession game;
+    game.initGameSession(2, 3, 10, 5);
+    game.gameCycle(20, 2.0);
+    
+    // Clean up allocated memory
+    for (GridUnit* unit : game.getGrid()) {
+        delete unit;
+    }
+
     return 0;
 }
